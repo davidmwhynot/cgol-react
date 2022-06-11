1b4ee20f369197f9ace42655736ebfa79cef70e3
David Whynot <davidmwhynot@gmail.com>
2022-06-11 01:16:04 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 export const CELL_WIDTH = 20;
 export const CELL_HEIGHT = 20;
 
-export const BOARD_WIDTH = 190;
-export const BOARD_HEIGHT = 100;
+export const BOARD_WIDTH = 95;
+export const BOARD_HEIGHT = 50;
 
 export const ALIVE_COLOR = '#ffffff';
 export const DEAD_COLOR = '#000000';
